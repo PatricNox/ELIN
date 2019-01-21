49e433afab8d256a25f0669e01f31f02a4b614cd
PatricNox <patricnox@gmail.com>
2019-01-21 20:53:11 +0100

    Implement Countdown

--- assets/js/core.js
+++ assets/js/core.js
@@ -73,3 +73,36 @@ key.addEventListener('click', (e) => {
     key.style.transform = rotateValue;
 
 })
+
+/** Countdown..er?  **/
+/*********************/
+function startTimer(duration) {
+    display = document.querySelector('#counter');
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds; 
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            start = Date.now() + 1000;
+        }
+    };
+
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
